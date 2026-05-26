89481ad65f781380f8be572dc529da4b5fbf7a0f
jaydenfyi <213395523+jaydenfyi@users.noreply.github.com>
2026-05-27 01:44:41 +0800

    Fix naming: scrollodex → scrolodex (single L) in filenames and URLs

--- Casks/scrolodex.rb
+++ Casks/scrolodex.rb
@@ -1,11 +1,11 @@
-cask "scrollodex" do
+cask "scrolodex" do
   version "0.0.0"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
 
-  url "https://github.com/jaydenfyi/scrollodex/releases/download/v#{version}/Scrolodex-#{version}.zip"
+  url "https://github.com/jaydenfyi/scrolodex/releases/download/v#{version}/Scrolodex-#{version}.zip"
   name "Scrolodex"
   desc "macOS menu-bar app for switching windows by scrolling"
-  homepage "https://github.com/jaydenfyi/scrollodex"
+  homepage "https://github.com/jaydenfyi/scrolodex"
 
   depends_on macos: ">= :ventura"
 
--- Formula/scrolodex.rb
+++ Formula/scrolodex.rb
@@ -1,7 +1,7 @@
-class Scrollodex < Formula
+class Scrolodex < Formula
   desc "macOS menu-bar app for switching windows by scrolling"
-  homepage "https://github.com/jaydenfyi/scrollodex"
-  url "https://github.com/jaydenfyi/scrollodex/archive/refs/tags/v0.0.0.tar.gz"
+  homepage "https://github.com/jaydenfyi/scrolodex"
+  url "https://github.com/jaydenfyi/scrolodex/archive/refs/tags/v0.0.0.tar.gz"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "MIT"
 
