b65a5ebccc4ef68a3ec284c67225935a28bd0ca8
jaydenfyi <213395523+jaydenfyi@users.noreply.github.com>
2026-05-27 02:39:15 +0800

    scrolodex 0.0.0

--- Casks/scrolodex.rb
+++ Casks/scrolodex.rb
@@ -1,6 +1,6 @@
 cask "scrolodex" do
   version "0.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "6921ace97110622daedf427df1fbc3a205d9b66e00141aadeb3cec9ef1550b7f"
 
   url "https://github.com/jaydenfyi/scrolodex/releases/download/v#{version}/Scrolodex-#{version}.zip"
   name "Scrolodex"
--- Formula/scrolodex.rb
+++ Formula/scrolodex.rb
@@ -2,7 +2,7 @@ class Scrolodex < Formula
   desc "macOS menu-bar app for switching windows by scrolling"
   homepage "https://github.com/jaydenfyi/scrolodex"
   url "https://github.com/jaydenfyi/scrolodex/archive/refs/tags/v0.0.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "447190f63581e2aac083bcd755730bea6dc64a4b217cc666ae64f3150cb5cccb"
   license "MIT"
 
   depends_on :xcode => ["15.0", :build]
